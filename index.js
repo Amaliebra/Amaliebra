1ad71d16df9f606b7a9dddf566307e1a3f4d89b7
Amaliebra <amaliebratland@gmail.com>
2025-01-28 20:21:38 +0100

    Fixed regex to align with the img tag in readme

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ async function fetchDogPictures() {
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
         const data = await response.json();
         const imageUrl = data.message;
-        const markdownImage = `<img src="${imageUrl}" alt="dog" height="10%"/>`;
+        const markdownImage = `<img src="${imageUrl}" alt="dog" height="10%" width="10%"/>`;
         const readme = fs.readFileSync('README.md', 'utf8');
 
         console.log('Fetched Image URL:', imageUrl);
 
-        const updatedReadme = readme.replace(/<img\s+src=".*?"\s+alt="dog"\s+width="20"\s*\/?>/i, markdownImage);
+        const updatedReadme = readme.replace(/<img\s+src=".*?"\s+alt="dog".*?\/>/i, markdownImage);
 
         fs.writeFileSync('README.md', updatedReadme);
         console.log('README.md updated with a new dog picture!');
     } catch(error) {
         console.error('Error fetching dog image :(', error);
+        process.exit(1);
     }
     
 }
